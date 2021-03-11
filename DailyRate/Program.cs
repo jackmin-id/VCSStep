202e6787900e8a8ab9a1a755f04e8ed0960ea933
jackmin-id <minjwus@gmail.com>
2021-03-10 20:05:12 -0700

    ch4

--- DailyRate/Program.cs
+++ DailyRate/Program.cs
@@ -12,19 +12,22 @@ namespace DailyRate
     {
         static void Main(string[] args)
         {
-            (new Program()).run();
+            //(new Program()).run();
+            Program p = new Program();
+            p.run();
         }
 
         public void run()
         {
-            double dailyRate = readDouble("Enter your daily rate: ");
+            double dailyRate1 = readDouble("Enter your daily rate: ");
             int noOfDays = readInt("Enter the number of days: ");
-            writeFee(calculateFee(dailyRate, noOfDays));
+            writeFee(calculateFee(dailyRate:dailyRate1, noOfDays));
         }
 
         private void writeFee(double p)
         {
             Console.WriteLine("The consultant's fee is: {0}", p * 1.1);
+            Console.WriteLine();
         }
 
         private double calculateFee(double dailyRate, int noOfDays)
